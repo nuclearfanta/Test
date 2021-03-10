b8ac9f59b1b4b7980d4162546b2dc60c3bf61eaa
nuclearfanta <you@example.com>
2021-03-10 17:51:32 +0200

    first commit

---
+++ IdeaProjects/HelloWorld/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+     public static void main(String[] args) {
+
+
+
+        double a = 0.45359237;
+        int b = 3;
+
+        double v = a * b;
+        System.out.println(v);
+
+
+    }
+}
